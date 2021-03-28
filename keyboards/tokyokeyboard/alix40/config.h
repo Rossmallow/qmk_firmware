3a5f3418db84605872908aadde280c0c820ec524
Ross Nelson <rossn430@gmail.com>
2021-03-28 12:01:30 -0500

    Custom Alix40 settings

--- keyboards/tokyokeyboard/alix40/config.h
+++ keyboards/tokyokeyboard/alix40/config.h
@@ -16,6 +16,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "config_common.h"
 
+#define TAPPING_TERM 200
+#define IGNORE_MOD_TAP_INTERRUPT
+#define TAPPING_FORCE_HOLD
+
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0x5143 // QC
 #define PRODUCT_ID      0x4134 // A4
